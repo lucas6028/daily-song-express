191032fdb23d098241a2078d6f67fe579ea4926e
haoping6028 <lucas60303@gmail.com>
2024-08-30 22:54:17 +0800

    add requirement of top aritist

--- server/src/routes/artistRoutes.ts
+++ server/src/routes/artistRoutes.ts
@@ -5,7 +5,12 @@ const router = Router();
 
 router.get("/myTop", async (req, res) => {
   try {
-    const data = await spotifyAPI.getMyTopArtists({ limit: 20 });
+    const { time_range = "medium_term", limit = 10, offset = 5 } = req.body;
+    const data = await spotifyAPI.getMyTopArtists({
+      time_range: time_range,
+      limit: limit,
+      offset: offset,
+    });
     console.log(data);
     res.json(data);
   } catch (err) {
