59723e75fac172aa0a08eef288726e922b54e7ed
haoping6028 <lucas60303@gmail.com>
2024-09-10 20:12:12 +0800

    change cookie max age to 0

--- server/src/routes/logoutRoutes.ts
+++ server/src/routes/logoutRoutes.ts
@@ -4,9 +4,12 @@ import spotifyAPI from "../config/spotifyConfig";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.clearCookie("access_token");
-  res.clearCookie("refresh_token");
-  res.clearCookie("profileId");
+  // res.clearCookie("access_token");
+  // res.clearCookie("refresh_token");
+  // res.clearCookie("profileId");
+  res.cookie("access_token", "", { maxAge: 0 });
+  res.cookie("refresh_token", "", { maxAge: 0 });
+  res.cookie("profileId", "", { maxAge: 0 });
 
   spotifyAPI.setAccessToken("");
   res.send("Log out successfully!");
