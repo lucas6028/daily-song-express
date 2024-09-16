de65ac98e455ffba701a7fa16839cf08e3ecbfa6
haoping6028 <lucas60303@gmail.com>
2024-09-16 12:10:10 +0800

    optimize trimName.test.ts

--- client/src/utils/__test__/trimName.test.ts
+++ client/src/utils/__test__/trimName.test.ts
@@ -1,20 +1,26 @@
-import { test, expect } from "vitest";
+import { test, expect, describe } from "vitest";
 import { trimName } from "../name";
 
-test("trim the song name after -", () => {
-  expect(trimName("這麼多年 - 電影《我想和你在一起》插曲")).toBe("這麼多年");
-});
+describe("trimName function", () => {
+  test("should trim the song name after hyphen (-)", () => {
+    expect(trimName("這麼多年 - 電影《我想和你在一起》插曲")).toBe("這麼多年");
+  });
 
-test("trim the song name after (", () => {
-  expect(trimName("3D (feat. Jack Harlow)")).toBe("3D");
-});
+  test("should trim the song name after parentheses ( )", () => {
+    const cases = [
+      { input: "3D (feat. Jack Harlow)", output: "3D" },
+      {
+        input: "A Beautiful Life(From the Netflix Film 'A Beautiful Life')",
+        output: "A Beautiful Life",
+      },
+    ];
 
-test("trim the song name after (", () => {
-  expect(
-    trimName("A Beautiful Life(From the Netflix Film 'A Beautiful Life')")
-  ).toBe("A Beautiful Life");
-});
+    cases.forEach(({ input, output }) => {
+      expect(trimName(input)).toBe(output);
+    });
+  });
 
-test("trim the name after noting", () => {
-  expect(trimName("Dream")).toBe("Dream");
+  test("should not trim the name if no delimiter is present", () => {
+    expect(trimName("Dream")).toBe("Dream");
+  });
 });
