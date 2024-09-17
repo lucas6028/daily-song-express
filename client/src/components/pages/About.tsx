9a929a99de4e7eb7c5a48c863b2dab551bd09e22
haoping6028 <lucas60303@gmail.com>
2024-09-17 20:52:59 +0800

    add Google Font API

--- client/src/components/pages/About.tsx
+++ client/src/components/pages/About.tsx
@@ -1,14 +1,14 @@
 import NavBar from '../ui/navbar/Navbar'
 import Footer from '../ui/footer/Footer'
 import { Container } from 'react-bootstrap'
-
+import styles from "../styles/About.module.css";
 
 function About() {
     return (
         <>
             <NavBar />
             <Container className='d-flex justify-content-center align-items-center'>
-                <p>這是我使用React Typescript 所做的網站</p>
+                <p className={styles.chocolateClassicalSansRegular}>這是我使用React Typescript 所做的網站</p>
             </Container>
             <Footer />
         </>
--- client/src/components/pages/Dashboard.tsx
+++ client/src/components/pages/Dashboard.tsx
@@ -32,6 +32,7 @@ export default function Dashboard() {
         };
 
         checkAuth();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     if (!isAuthenticated) {
